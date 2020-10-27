cba94370d6f015762a5cb64c813cc60a18a2b8f0
Renato Mateus <renatomateusx@gmail.com>
2020-10-27 14:43:15 -0300

    Implementation

---
+++ application/src/main/java/com/renatomateusx/arch/main/Boot.java
@@ -0,0 +1,9 @@
+package com.renatomateusx.arch.main;
+
+
+@SringBootApplication
+public class Boot {
+    public static void main(String[] args) {
+        SpringApplication.run(Boot.class, args);
+    }
+}
