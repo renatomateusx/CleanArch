26d6430d624929d8b81a7b99e41d7cecaf3e67ea
Renato Mateus <renatomateusx@gmail.com>
2020-10-27 11:44:14 -0300

    First Commit

---
+++ src/test/java/com/renatomateusx/arch/CleanArch/CleanArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renatomateusx.arch.CleanArch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
