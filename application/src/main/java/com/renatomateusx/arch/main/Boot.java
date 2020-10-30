18805e33f121aec4ac3e0052ff963c32db33929a
Renato Mateus <renatomateusx@gmail.com>
2020-10-30 10:32:27 -0300

    Implementação do Java - Iniciando com as controllers a partir daqui

--- application/src/main/java/com/renatomateusx/arch/main/Boot.java
+++
@@ -1,9 +0,0 @@
-package com.renatomateusx.arch.main;
-
-
-@SringBootApplication
-public class Boot {
-    public static void main(String[] args) {
-        SpringApplication.run(Boot.class, args);
-    }
-}
